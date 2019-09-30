ca3521d98b4ba6f8cfbc76e85cbd79cf5efc48c3
Tolyan Kulinich <kulinich@rivne.itstep.org>
2019-09-30 12:13:50 +0300

    30.09

---
+++ 30.09.2019/30.09.2019/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30._09._2019
+{
+    class Program
+    {
+        class Square : IDisposable
+        {
+            public void Show(ConsoleColor color)
+            {
+                Console.ForegroundColor = color;
+                Console.WriteLine(@"      ### 
+    ##   ##
+  ##       ##   
+##    ###    ## 
+    ##   ##  
+  ##       ##
+##           ##
+");
+            }
+            public void Dispose()
+            {
+                Console.ResetColor();
+            }
+
+            public void Show()
+            {
+                Console.WriteLine(@"      ### 
+    ##   ##
+  ##       ##   
+##    ###    ## 
+    ##   ##  
+  ##       ##
+##           ##
+");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Square a = new Square();
+            Console.WriteLine("Enter color: ");
+            string colorUse = Console.ReadLine();
+
+            try
+            {
+                a.Show((ConsoleColor)Enum.Parse(typeof(ConsoleColor), colorUse));
+            }
+            finally
+            {
+                a.Dispose();
+            }
+
+            a.Show();
+        }
+    }
+}
